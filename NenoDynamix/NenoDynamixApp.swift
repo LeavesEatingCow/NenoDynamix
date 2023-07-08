45a426296146d220e993163b1fd699ca6b69293e
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-07-08 16:26:00 -0400

    Initial Commit

---
+++ NenoDynamix/NenoDynamixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NenoDynamixApp.swift
+//  NenoDynamix
+//
+//  Created by Daniel Maliro on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct NenoDynamixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
